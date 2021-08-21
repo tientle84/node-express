f7c10e70595f604cb5a5e93592de365ced268129
tientle84 <alctdl00@gmail.com>
2021-08-20 20:19:58 -0400

    new update

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
-const productRouter = require('./routes/productRouter');
+const express = require("express");
+const morgan = require("morgan");
+const campsiteRouter = require("./routes/campsiteRouter");
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/products', productRouter);
+app.use("/campsites", campsiteRouter);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
 
 app.listen(port, hostname, () => {
